00b60675e37e26077a50849190d7bfbb3b0da962
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-10 00:24:39 +0900

    /문자열 내림차순으로 배치

--- Solution/Solution.go
+++ Solution/Solution.go
@@ -1,6 +1,21 @@
 package Solution
 
 import "fmt"
+import "sort"
+//문자열 내림차순으로 배치
+func solution3(s string) string { 
+	var result string 
+	var arr []int 
+	for i:=0 ; i<len(s); i++ { 
+		arr = append(arr, int(s[i])) 
+		} 
+		sort.Sort(sort.Reverse(sort.IntSlice(arr))) 
+		for i:=0 ; i<len(arr); i++ { 
+			result +=string(arr[i]) 
+			} 
+			return result 
+		}
+
 //단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환
 func Solution(s string) string{ 
 	var a string
@@ -44,6 +59,7 @@ func Solution(s string) string{
     func Result() { 
 		fmt.Printf(Solution("dada"))
 		// fmt.Printf( Benchmark_solution(3,20,4))
-		Multiplication()
+		fmt.Printf(solution3("Zbcdefg"))
+		
 		 }
 	
