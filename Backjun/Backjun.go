c6e3c010f1d5420e474c9bf01979c27cca92d55a
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-24 23:34:49 +0900

    add

--- Backjun/Backjun.go
+++ Backjun/Backjun.go
@@ -1,6 +1,11 @@
 package Backjun
 
-import "fmt"
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func Back1(){
 	//첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
@@ -25,6 +30,27 @@ for i := n - 1; i >= 0; i-- {
 fmt.Println(cnt)
 
 }
+
+func Back2() {
+	//문제
+	//전화걸고싶은 번호 있다면 숫자 하나누른다음 금속핀이 있는곳까지 시계방향으로 돌려야한다
+	//숫자 하나를 누르면 다이얼이 처음위치로 돌아감
+	//숫자 1을 걸려면 총 2초
+	//1보다 큰수를 거는데 시간은 이보다 더걸린다
+	//한칸옆에 있는 숫자를 걸기위해서는 1초씩 걸린다
+	//상근이할머니 는 전화번호를 각 숫자에 해당하는 문자로외운다즉 어떤 단어를 걸때 각 알파벳에 해달하는 숫자를 걸면댄다
+	var word string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscanln(reader, &word)
+
+	var seconds = []int{3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10}
+	var result int
+	for i := 0; i < len(word); i++ {
+		result += seconds[int(word[i])-65]
+	}
+	fmt.Println(result,"result")
+}
 func Result(){
 	Back1()
+	Back2()
 }
